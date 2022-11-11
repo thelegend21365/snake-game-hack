9435e8e98402078dc8ca583fc24135c8c8635bdc
thelegend21365 <117957329+thelegend21365@users.noreply.github.com>
2022-11-11 01:20:24 +0000

    Add files via upload

---
+++ something.js
@@ -0,0 +1,73 @@
+//snake game hacking script
+
+function getFoodPosition_x() {
+    console.log(ax)
+}
+function getFoodPosition_y() {
+    console.log(ay)
+}
+function getSnakePosition_x() {
+    console.log(px)
+}
+function getSnakePosition_y() {
+    console.log("hello")
+}
+function tailSize(tail_size) {
+    tail = tail_size
+    return tail
+}
+function snakeAccelerate() {
+    return window.onload();
+}
+function snakePosition_x(snake_position_x) {
+    px = snake_position_x;
+    return px;
+}
+function snakePosition_y(snake_position_y) {
+    py = snake_position_y;
+    return py;
+}
+function snakeMovementAutomation_x(x_velocity) {
+    while (true) {
+    xv = x_velocity;
+    return xv;
+    }
+}
+function snakeMovementAutomation_y(y_velocity) {
+    while (true) {
+    yv = y_velocity;
+    };
+    return yv;
+}
+function foodPosition_x(food_position_x) {
+    ax = food_position_x
+    return ax;
+}
+function foodPosition_y(food_position_y) {
+    ay = food_position_y
+    return ay;
+}
+function changeDirection_x(x_velocity) {
+    if (x_velocity == 0) {
+        xv = 0
+    }
+    if (x_velocity == 1){
+        xv = 1
+    }
+    if (x_velocity == -1) {
+        xv = -1
+    }
+}
+function changeDirection_y(y_velocity) {
+    if (y_velocity == 0) {
+        yv = 0
+    }
+    if (y_velocity == 1){
+        yv = 1
+    }
+    if (y_velocity == -1) {
+        yv = -1
+    }
+}
+
+    
